9a77a693a903226dc9254b01a6126f6731b5fb12
Morgan <msswanso@calpoly.edu>
2021-02-04 23:05:09 -0800

    added a demo file to test

---
+++ fitfarmer/diagnose.py
@@ -0,0 +1,13 @@
+
+
+
+def diagnose_diabetes(a1c):
+    
+
+    
+def diagnose(patient):
+    '''returns a list of diagnostics given a dictionary of patient data'''
+    diagnostics = []
+    diagnostics.append(diagnose_diabetes(patient['A1C Column']))
+    return diagnostics
+    
