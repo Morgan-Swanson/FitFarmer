88e6f47bb21f20123e9e05593245c72b4003b91c
Roxanne Miller <roxanne@MacBook-Pro.local>
2021-02-05 00:39:53 -0800

    reformat csv

--- fitfarmer/data.py
+++ fitfarmer/data.py
@@ -6,7 +6,7 @@ def get_patient_csv_data(path):
                                   'FPG', 'HbA1c', 'CRP', 'PulseOx', 'ServGrain', 'ServFruit',
                                   'ServVeg', 'ServProtein', 'ServDairy', 'TotalCalories', 
                                   'EnergyCHO', 'EnergyPro', 'EnergyFat', 'VitD', 'VitE',
-                                  'VitA', 'VitK', 'VitB12', 'Folate'], header=0)
+                                  'VitA', 'VitK', 'VitB12', 'Folate'], header=0, index_col=False)
     patient_data = df.to_dict(orient='records')
     return patient_data
 
