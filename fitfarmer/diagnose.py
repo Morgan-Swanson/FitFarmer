0d59e2508b63a849534984c490b19e74df90d40c
Morgan <msswanso@calpoly.edu>
2021-02-04 23:44:59 -0800

    added enum but didn't integrate yet

--- fitfarmer/diagnose.py
+++ fitfarmer/diagnose.py
@@ -1,11 +1,26 @@
+class Diseases(Enum):
+    # Diabetes
+    DIABETIC = 1
+    PREDIABETIC = 2
+    
+    # Obesity
+    UNDERWEIGHT = 3
+    OVERWEIGHT = 4
+    OBESE = 5
 def diagnose_diabetes(a1c):
     if a1c > 6.5:
         return 'diabetic'
     elif a1c > 5.7:
         return 'prediabetic'
-    else:
-        return None
 
+def diagnose_obesity(bmi):
+    if bmi < 18.5:
+        return 'underweight'
+    elif bmi > 30:
+        return 'obese'
+    elif bmi >= 25:
+        return 'overweight'
+    
     
 def diagnose(patient):
     '''returns a list of diagnostics given a dictionary of patient data'''
