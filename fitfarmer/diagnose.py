f34a85c1719696e191ab7000c4a2c8a100816eeb
Morgan <msswanso@calpoly.edu>
2021-02-05 00:37:16 -0800

    diagnosing obesity and diabetes

--- fitfarmer/diagnose.py
+++ fitfarmer/diagnose.py
@@ -1,4 +1,6 @@
-class Diseases(Enum):
+from enum import Enum
+
+class Disease(Enum):
     # Diabetes
     DIABETIC = 1
     PREDIABETIC = 2
@@ -7,24 +9,27 @@ class Diseases(Enum):
     UNDERWEIGHT = 3
     OVERWEIGHT = 4
     OBESE = 5
+    
+    
 def diagnose_diabetes(a1c):
     if a1c > 6.5:
-        return 'diabetic'
+        return Disease.DIABETIC
     elif a1c > 5.7:
-        return 'prediabetic'
+        return Disease.PREDIABETIC
 
 def diagnose_obesity(bmi):
     if bmi < 18.5:
-        return 'underweight'
+        return Disease.UNDERWEIGHT
     elif bmi > 30:
-        return 'obese'
+        return Disease.OBESE
     elif bmi >= 25:
-        return 'overweight'
-    
+        return Disease.OVERWEIGHT
     
 def diagnose(patient):
     '''returns a list of diagnostics given a dictionary of patient data'''
     diagnostics = []
     diagnostics.append(diagnose_diabetes(patient['HbA1c']))
+    diagnostics.append(diagnose_obesity(patient['BMI']))
+    diagnostics = [d for d in diagnostics if d is not None]
     return diagnostics
     
