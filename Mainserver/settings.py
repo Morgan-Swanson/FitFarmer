367ac0043801ad645b52189906f1610df047d15f
morgan <msswanso@calpoly.edu>
2021-03-05 02:30:51 -0800

    bugfix 8

--- Mainserver/settings.py
+++ Mainserver/settings.py
@@ -70,12 +70,11 @@ WSGI_APPLICATION = 'Mainserver.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(conn_max_age=500)
 }
 
 
@@ -117,10 +116,7 @@ django_heroku.settings(locals())
 
 STATIC_URL = '/static/'
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
